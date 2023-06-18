9d44f98f771a4a5f21478d15e7003e6b824671dc
cbjayanath <66734455+cbjayanath@users.noreply.github.com>
2023-06-18 10:59:03 +0530

    added login page and db connection

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http;
 
+use App\Http\Middleware\UserAuth;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -18,9 +19,14 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrustProxies::class,
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
+        \Illuminate\Session\Middleware\StartSession::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        UserAuth::class,
+        
+
+        
     ];
 
     /**
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,19 +6,19 @@
     <div class="row justify-content-center" style="margin-top: 50px"> <!-- Added justify-content-center class -->
         <div class="col-sm-4">
         
-            <form>
-              
+            <form action="/login" method="POST">
+                @csrf
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
                     
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input type="password" name="password" class="form-control" id="exampleInputPassword1">
                 </div>
 
-                <button type="submit" class="btn btn-primary col-sm-12">Submit</button>
+                <button type="submit" class="btn btn-primary col-sm-12">Login</button>
             </form>
         </div>
     </div>
